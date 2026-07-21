dd70b40d9def2f336843696137c2df63a71bf8e6
Drakonkinst <drakonkinstmc@gmail.com>
2026-07-21 07:55:40 -0700

    Add sample scene and audio management

--- global.gd
+++ global.gd
@@ -12,16 +12,44 @@ var environment := AudioServer.get_bus_index("Environment")
 var audio: AudioManager
 
 func _ready() -> void:
-	_load_config()
-	print("music=", music, ", ui=", ui, ", environment=", environment)
+    _load_config()
+    print("music=", music, ", ui=", ui, ", environment=", environment)
 
 func _load_config() -> void:
-	var err := config.load(CONFIG_PATH)
-	if err != OK:
-		return
-	# if config.has_section_key("category", "setting") -> config.get_value("category", "setting")
-	config.save(CONFIG_PATH)   
+    var err := config.load(CONFIG_PATH)
+    if err != OK:
+        return
+    if config.has_section_key("audio", "music"):
+            set_music_volume(config.get_value("audio", "music"), false)
+    if config.has_section_key("audio", "ui"):
+        set_ui_volume(config.get_value("audio", "ui"), false)
+    if config.has_section_key("audio", "environment"):
+        set_environment_volume(config.get_value("audio", "environment"), false)
+    config.save(CONFIG_PATH)   
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func set_music_volume(value: float, save: bool = true) -> void:
+    AudioServer.set_bus_volume_db(music, linear_to_db(value))
+    if save:
+        config.set_value("audio", "music", value)
+        config.save(CONFIG_PATH)
+
+func set_ui_volume(value: float, save: bool = true) -> void:
+    AudioServer.set_bus_volume_db(ui, linear_to_db(value))
+    if save:
+        config.set_value("audio", "ui", value)
+        config.save(CONFIG_PATH)
+
+func set_environment_volume(value: float, save: bool = true) -> void:
+    AudioServer.set_bus_volume_db(environment, linear_to_db(value))
+    if save:
+        config.set_value("audio", "environment", value)
+        config.save(CONFIG_PATH)
+
+func get_music_volume() -> float:
+    return db_to_linear(AudioServer.get_bus_volume_db(music))
+
+func get_environment_volume() -> float:
+    return db_to_linear(AudioServer.get_bus_volume_db(environment))
+
+func get_ui_volume() -> float:
+    return db_to_linear(AudioServer.get_bus_volume_db(ui))
--- main.gd
+++ main.gd
@@ -10,34 +10,34 @@ var game: Game
 var menu: MainMenu
 
 func _ready() -> void:
-	Global.audio = audio
-	
-	# Dev shortcut - just go directly to new game for now
-	# init_main_menu()
-	start_new_game()
-	
+    Global.audio = audio
+    
+    # Dev shortcut - just go directly to new game for now
+    # init_main_menu()
+    start_new_game()
+    
 func init_main_menu() -> void:
-	menu = main_menu_scene.instantiate()
-	menu.start_game.connect(start_new_game)
-	add_child(menu)
+    menu = main_menu_scene.instantiate()
+    menu.start_game.connect(start_new_game)
+    add_child(menu)
 
 func start_new_game() -> void:
-	if menu != null:
-		remove_child(menu)
-		menu = null
-	game = game_scene.instantiate()
-	game.exit_game.connect(exit_to_menu)
-	game.restart_game.connect(restart)
-	Global.game = game
-	add_child(game)
+    if menu != null:
+        remove_child(menu)
+        menu = null
+    game = game_scene.instantiate()
+    game.exit_game.connect(exit_to_menu)
+    game.restart_game.connect(restart)
+    Global.game = game
+    add_child(game)
 
 func exit_to_menu() -> void:
-	if game != null:
-		remove_child(game)
-		game = null
-	init_main_menu()
+    if game != null:
+        remove_child(game)
+        game = null
+    init_main_menu()
 
 func restart() -> void:
-	exit_to_menu()
-	start_new_game()
-		
+    exit_to_menu()
+    start_new_game()
+        
