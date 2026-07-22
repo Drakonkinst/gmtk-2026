59e3b0358cb066414ac54456e8d88a56e19fb3e1
Drakonkinst <drakonkinstmc@gmail.com>
2026-07-21 18:20:13 -0700

    Fix indentation

--- audio/audio_randomizer2d.gd
+++ audio/audio_randomizer2d.gd
@@ -7,9 +7,9 @@ class_name AudioRandomizer2D
 @export var max_pitch: float = 1.1
 
 func play_random(source: String = "") -> void:
-	if audio_tracks.size() <= 0:
-		print("WARN: No audio found")
-		return
-	stream = audio_tracks[randi() % audio_tracks.size()]
-	pitch_scale = randf_range(min_pitch, max_pitch)
-	play()
+    if audio_tracks.size() <= 0:
+        print("WARN: No audio found")
+        return
+    stream = audio_tracks[randi() % audio_tracks.size()]
+    pitch_scale = randf_range(min_pitch, max_pitch)
+    play()
--- ui/menu/main_menu.gd
+++ ui/menu/main_menu.gd
@@ -5,5 +5,5 @@ class_name MainMenu
 signal start_game
 
 func _ready() -> void:
-	get_tree().paused = false
-	# Show menus and connect buttons
+    get_tree().paused = false
+    # Show menus and connect buttons
